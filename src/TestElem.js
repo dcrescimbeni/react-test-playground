f1d441265cd8562c2f9ab474d03297b9c238ad14
Dino <dc@estudiocrescimbeni.com.ar>
2021-10-23 19:12:20 -0300

    initial commit

---
+++ src/TestElem.js
@@ -0,0 +1,10 @@
+function TestElem({ displayText }) {
+  return (
+    <div>
+      <p>{displayText}</p>
+      <a href="http://google.com">Google</a>
+    </div>
+  );
+}
+
+export default TestElem;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TestElem from './TestElem';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <TestElem displayText="random" />
   </React.StrictMode>,
   document.getElementById('root')
 );
